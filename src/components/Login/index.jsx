cfaf1a14b4fb46be5a26828b6bad8b2a1e0fa082
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-03-04 18:04:56 +0100

    Login TF

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -2,25 +2,30 @@ import "./login.scss"
 import React from 'react';
 import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 import HttpsOutlinedIcon from '@material-ui/icons/HttpsOutlined';
-import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
+import { Grid, TextField, FormControl, InputAdornment, InputLabel, Input, Button } from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import { makeStyles } from '@material-ui/core/styles';
 
 
-{/* botones */}
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
+// botones
+const useStyles = makeStyles(theme => ({
+    grid: {
+        width: '90%',
     },
-  },
+    textField: {
+        width: '100%',
+    }
 }));
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         '& > *': {
+//             margin: theme.spacing(1),
+//         },
+//     },
+// }));
+
+
 
 /*
 const useStyles = makeStyles((theme) => ({
@@ -36,8 +41,8 @@ export const Login = () => {
 
     return (
         <div id="login">
-           <div className="login__card">
-                <div className="login__card-header"></div> 
+            <div className="login__card">
+                <div className="login__card-header"></div>
 
                 <div className="login__card-body">
                     <p className="login__card-body-title">
@@ -59,36 +64,36 @@ export const Login = () => {
                         <input class="login__form-button" type="submit" value="Login"/>
                     </form> 
                     */}
-                    
+
                     <div className={classes.margin}>
                         <Grid container spacing={1} alignItems="flex-end">
-                        <Grid item>
-                            <PersonOutlineOutlinedIcon fontSize="large" />
-                        </Grid>
-                        <Grid item>
-                            <TextField id="input-with-icon-grid" label="Email" />
-                        </Grid>
+                            <Grid item>
+                                <PersonOutlineOutlinedIcon fontSize="large" />
+                            </Grid>
+                            <Grid item classes={{ root: classes.grid }}>
+                                <TextField classes={{ root: classes.textField }} id="input-with-icon-grid" label="Email" />
+                            </Grid>
                         </Grid>
                     </div>
 
                     <div className={classes.margin}>
                         <Grid container spacing={1} alignItems="flex-end">
-                        <Grid item>
-                            <HttpsOutlinedIcon fontSize="large" />
-                        </Grid>
-                        <Grid item>
-                            <TextField id="input-with-icon-grid" label="Password" />
-                        </Grid>
+                            <Grid item>
+                                <HttpsOutlinedIcon fontSize="large" />
+                            </Grid>
+                            <Grid item classes={{ root: classes.grid }}>
+                                <TextField classes={{ root: classes.textField }} id="input-with-icon-grid" label="Password" />
+                            </Grid>
                         </Grid>
                     </div>
-                  
+
                     <Button variant="contained" color="primary">
                         Login
                     </Button>
                 </div>
 
                 <div className="login__card-footer"></div>
-           </div>
+            </div>
         </div>
     )
 }
